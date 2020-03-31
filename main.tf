074b9bcec7977411299f1ef9bb46af994eab9a28
Sean Hellum <seanhellum45@gmail.com>
2020-03-31 18:51:34 +0000

    use variables

--- main.tf
+++ main.tf
@@ -1,18 +1,23 @@
+variable "region" {
+    default = "us-east-2"
+}
+variable "ami" {
+    default = "ami-0fc20dd1da406780b"
+}
 provider "aws" {
   profile = "default"
-  region  = "us-east-2"
+  region  = var.region
 }
-
 resource "aws_instance" "Gitpod-AWS" {
-  ami           = "ami-0fc20dd1da406780b"
+  ami           = var.ami
   instance_type = "t2.micro"
   provisioner "remote-exec" {
     inline = [
-        "./clone.sh",
-        "./deps.sh",
-        "helm repo add charts.gitpod.io https://charts.gitpod.io",
-        "helm dep update",
-        "helm upgrade --install $(for i in $(cat configuration.txt); do echo -e \"-f $i\"; done) gitpod ."
+      "./clone.sh",
+      "./deps.sh",
+      "helm repo add charts.gitpod.io https://charts.gitpod.io",
+      "helm dep update",
+      "helm upgrade --install $(for i in $(cat configuration.txt); do echo -e \"-f $i\"; done) gitpod ."
     ]
   }
 }
