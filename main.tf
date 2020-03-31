8658b9bfb6f7e6b610b3e31ec78ddcf8aaec75e0
Sean Hellum <seanhellum45@gmail.com>
2020-03-31 20:00:12 +0000

    format code style

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 variable "aws" {
-    type = object({
-        region = string
-        ami = string
-        profile = string
-        key_name = string
-        instance_type = string
-    })
-    default = {
-        region = "us-east-2"
-        ami = "ami-0fc20dd1da406780b"
-        profile = "default"
-        key_name = "ubuntu"
-        instance_type = "t2.micro"
-    }
+  type = object({
+    region        = string
+    ami           = string
+    profile       = string
+    key_name      = string
+    instance_type = string
+  })
+  default = {
+    region        = "us-east-2"
+    ami           = "ami-0fc20dd1da406780b"
+    profile       = "default"
+    key_name      = "ubuntu"
+    instance_type = "t2.micro"
+  }
 }
 provider "aws" {
   profile = var.aws.profile
@@ -21,5 +21,5 @@ provider "aws" {
 resource "aws_instance" "Gitpod-AWS" {
   ami           = var.aws.ami
   instance_type = var.aws.instance_type
-  key_name = var.aws.key_name
+  key_name      = var.aws.key_name
 }
