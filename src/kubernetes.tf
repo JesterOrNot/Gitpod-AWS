377f07c7f18fb883c9cb7a1c919b58560bb2f921
Sean Hellum <seanhellum45@gmail.com>
2020-04-02 20:01:48 +0000

    change names

--- src/kubernetes.tf
+++ src/kubernetes.tf
@@ -2,7 +2,7 @@
 data "aws_availability_zones" "available" {}
 
 # To use EKS one needs a VPC or Virtual Private Cloud for base networking and this adds it
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "gitpod" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -12,12 +12,12 @@ resource "aws_vpc" "demo" {
 }
 
 # This will route external traffic through internet gateway
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "gitpod" {
   count = 2
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
   cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.demo.id
+  vpc_id            = aws_vpc.gitpod.id
 
   tags = {
     "Name"                                                 = var.kubernetes.vpc-name
@@ -26,35 +26,35 @@ resource "aws_subnet" "demo" {
 }
 
 # This is our Internet Gateway
-resource "aws_internet_gateway" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_internet_gateway" "gitpod" {
+  vpc_id = aws_vpc.gitpod.id
 
   tags = {
-    Name = "terraform-eks-demo"
+    Name = "terraform-eks-gitpod"
   }
 }
 
 # Define  Routes
-resource "aws_route_table" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_route_table" "gitpod" {
+  vpc_id = aws_vpc.gitpod.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo.id
+    gateway_id = aws_internet_gateway.gitpod.id
   }
 }
 
 # Associations for route table
-resource "aws_route_table_association" "demo" {
+resource "aws_route_table_association" "gitpod" {
   count = 2
 
-  subnet_id      = aws_subnet.demo[count.index].id
-  route_table_id = aws_route_table.demo.id
+  subnet_id      = aws_subnet.gitpod[count.index].id
+  route_table_id = aws_route_table.gitpod.id
 }
 
 # Allow EKS to interact with AWS stuffs
-resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-cluster"
+resource "aws_iam_role" "gitpod-node" {
+  name = "terraform-eks-gitpod-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -73,22 +73,22 @@ POLICY
 }
 
 # Attatchments
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "gitpod-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.demo-node.name
+  role       = aws_iam_role.gitpod-node.name
 }
 
 # Attatchments
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "gitpod-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.demo-node.name
+  role       = aws_iam_role.gitpod-node.name
 }
 
 # Networking Access to Kubernetes
-resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
+resource "aws_security_group" "gitpod-cluster" {
+  name        = "terraform-eks-gitpod-cluster"
   description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.demo.id
+  vpc_id      = aws_vpc.gitpod.id
 
   egress {
     from_port   = 0
@@ -98,33 +98,33 @@ resource "aws_security_group" "demo-cluster" {
   }
 
   tags = {
-    Name = "terraform-eks-demo"
+    Name = "terraform-eks-gitpod"
   }
 }
 
 # Allow me to Access Cluster from a workstation
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+resource "aws_security_group_rule" "gitpod-cluster-ingress-workstation-https" {
   cidr_blocks       = ["34.82.79.165/32"]
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.demo-cluster.id
+  security_group_id = aws_security_group.gitpod-cluster.id
   to_port           = 443
   type              = "ingress"
 }
 
 # The Kubernetes Cluster!
-resource "aws_eks_cluster" "demo" {
+resource "aws_eks_cluster" "gitpod" {
   name     = var.kubernetes.cluster-name
-  role_arn = aws_iam_role.demo-node.arn
+  role_arn = aws_iam_role.gitpod-node.arn
 
   vpc_config {
-    security_group_ids = [aws_security_group.demo-cluster.id]
-    subnet_ids         = aws_subnet.demo.*.id
+    security_group_ids = [aws_security_group.gitpod-cluster.id]
+    subnet_ids         = aws_subnet.gitpod.*.id
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.gitpod-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.gitpod-cluster-AmazonEKSServicePolicy,
   ]
 }
