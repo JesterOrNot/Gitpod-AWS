4c09f3d7598a7650b12e6c0125eb8674200118fe
Restyled.io <commits@restyled.io>
2020-04-05 23:48:45 +0000

    Restyled by shfmt

--- scripts/install-gitpod.sh
+++ scripts/install-gitpod.sh
@@ -7,19 +7,19 @@ terraform init
 did_fail="failed"
 echo 'yes' | terraform apply "$@" || did_fail=""
 if [ -z "$did_fail" ]; then
-	echo 'yes' | terraform destroy
-	exit
+  echo 'yes' | terraform destroy
+  exit
 fi
 aws eks --region "$(cat <(terraform output region))" update-kubeconfig --name "$(cat <(terraform output cluster_name))"
 kubectl apply -f <(terraform output config_map_aws_auth)
 if ! [ -d self-hosted ]; then
-	git clone "https://github.com/gitpod-io/self-hosted.git"
+  git clone "https://github.com/gitpod-io/self-hosted.git"
 fi
 cd self-hosted || exit
 kubectl create -f utils/helm-2-tiller-sa-crb.yaml
 helm repo add charts.gitpod.io "https://charts.gitpod.io"
 helm dep update
-cat << EOF > values.yaml
+cat <<EOF >values.yaml
 gitpod:
   hostname: $(kubectl get svc --namespace default proxy --template "{{ range (index .status.loadBalancer.ingress 0) }}{{.}}{{ end }}")
   components:
@@ -27,9 +27,9 @@ gitpod:
       loadBalancerIP: null
 EOF
 if [ -f "configuration.txt" ]; then
-	helm upgrade --install "$(for i in "$(cat configuration.txt)"; do echo -e "-f $i"; done)" gitpod .
+  helm upgrade --install "$(for i in "$(cat configuration.txt)"; do echo -e "-f $i"; done)" gitpod .
 else
-	helm install gitpod .
+  helm install gitpod .
 fi
 cd .. || exit
 rm -rf self-hosted
