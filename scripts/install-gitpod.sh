37e2f4f12865ee653ef72552269da1d5d0f65308
Restyled.io <commits@restyled.io>
2020-04-21 14:59:08 +0000

    Restyled by shellharden

--- scripts/install-gitpod.sh
+++ scripts/install-gitpod.sh
@@ -5,8 +5,8 @@ set -x
 cd src || exit
 terraform init
 did_fail="failed"
-echo 'yes' | terraform apply $@ || did_fail=""
-if [ -z $did_fail ]; then
+echo 'yes' | terraform apply "$@" || did_fail=""
+if [ -z "$did_fail" ]; then
 	echo 'yes' | terraform destroy
 	exit
 fi
@@ -24,12 +24,12 @@ read -p "What is your domain URL (e.g. example.com): " domain
 read -p "What is your Git host URL: (e.g. github.com)" hosturl
 read -p "What is your oauth client id: " clientId
 read -p "What is the client secret: " clientSecret
-if [ $provider = "GH" ]; then
+if [ "$provider" = "GH" ]; then
     providerType="GitHub"
 else
     providerType="GitLab"
 fi
-if [ $provider = "GH" ]; then
+if [ "$provider" = "GH" ]; then
     settingsUrl="https://github.com/settings/connections/applications/$clientId"
 else
     settingsUrl="gitlab.com/profile/applications"
@@ -53,7 +53,7 @@ gitpod:
   installPodSecurityPolicies: true
 EOF
 if [ -f "configuration.txt" ]; then
-	helm upgrade --install $(for i in $(cat configuration.txt); do echo -e "-f $i"; done) gitpod .
+	helm upgrade --install "$(for i in "$(cat configuration.txt)"; do echo -e "-f $i"; done)" gitpod .
 else
 	helm install gitpod .
 fi
