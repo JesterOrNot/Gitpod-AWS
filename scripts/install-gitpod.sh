906dbee2031cd36792ac9c3237a19d2e46a06eab
Sean Hellum <seanhellum45@gmail.com>
2020-04-29 20:10:58 +0000

    Fix websocket issue

--- scripts/install-gitpod.sh
+++ scripts/install-gitpod.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 set -ex
-
 cd src || exit
-terraform init
-did_fail="failed"
-# Take varriardic arguments from $1 on, the or statement will be used for error handling incase the install fails
-echo 'yes' | terraform apply "$@" || did_fail=""
-if [ -z "$did_fail" ]; then
-  printf "\x1b[31mSomething went wrong during the installation of Gitpod Self Hosted for AWS. This should not happen. Destroying any dangling infrastructure. Please file an issue at https://github.com/gitpod-io/self-hosted\x1b[m\n"
-  echo 'yes' | terraform destroy
-  exit
+if [[ ! "$@" =~ '--dev' ]]; then
+    terraform init
+    did_fail="failed"
+    # Take varriardic arguments from $1 on, the or statement will be used for error handling incase the install fails
+    echo 'yes' | terraform apply "$@" || did_fail=""
+    if [ -z "$did_fail" ]; then
+    printf "\x1b[31mSomething went wrong during the installation of Gitpod Self Hosted for AWS. This should not happen. Destroying any dangling infrastructure. Please file an issue at https://github.com/gitpod-io/self-hosted\x1b[m\n"
+    echo 'yes' | terraform destroy
+    exit
+    fi
+    # Configure local machine for the kubernetes cluseter
+    aws eks --region "$(cat <(terraform output region))" update-kubeconfig --name "$(cat <(terraform output cluster_name))"
+    kubectl apply -f <(terraform output config_map_aws_auth)
+else
+    aws eks --region "$(cat <(terraform output region))" update-kubeconfig --name "$(cat <(terraform output cluster_name))"
+    # kubectl apply -f <(terraform output config_map_aws_auth)
 fi
-# Configure local machine for the kubernetes cluseter
-aws eks --region "$(cat <(terraform output region))" update-kubeconfig --name "$(cat <(terraform output cluster_name))"
-kubectl apply -f <(terraform output config_map_aws_auth)
 # Clone self-hosted if it doesn't exist
 if ! [ -d self-hosted ]; then
   git clone "https://github.com/gitpod-io/self-hosted.git"
 fi
 cd self-hosted || exit
 # Necassary Kubernetes stuffs
-kubectl create -f utils/helm-2-tiller-sa-crb.yaml
+kubectl create -f utils/helm-2-tiller-sa-crb.yaml || echo 'already configured'
 helm repo add charts.gitpod.io "https://charts.gitpod.io"
 helm dep update
 # Get information
@@ -46,9 +50,9 @@ gitpod:
   hostname: $domain
   components:
     proxy:
-      loadBalancerIP: "$domain"
+      loadBalancerIP: null
   authProviders:
-  - id: "My ID"
+  - id: "Github"
     host: "$hosturl"
     protocol: "https"
     type: "$providerType"
@@ -58,14 +62,19 @@ gitpod:
       callBackUrl: "https://$domain/auth/github/callback"
       settingsUrl: "$settingsUrl"
   installPodSecurityPolicies: true
+
+docker-registry:
+  enabled: true
+
+gitpod_selfhosted:
+  variants:
+    customRegistry: false
 EOF
-# If self-hosted already exists there is a chance that there is extra configuration for self-hosted which the following honors
-if [ -f "configuration.txt" ]; then
-  helm upgrade --install $(for i in $(cat configuration.txt); do echo -e "-f $i"; done) gitpod .
-else
-  helm install gitpod .
-fi
-# cleanup
+echo 'values.yaml' >configuration.txt
+helm upgrade --install $(for i in $(cat configuration.txt); do echo -e "-f $i"; done) gitpod .
+real_url=$(kubectl get svc | grep -E '^proxy' | awk '{print $4}')
+sed -i "2s/.*/  hostname: $real_url/" values.yaml
+sed -i "14s/.*/      callBackUrl: \"https:\/\/$real_url\/auth\/github\/callback\"/" values.yaml
+helm upgrade --install $(for i in $(cat configuration.txt); do echo -e "-f $i"; done) gitpod .
 cd .. || exit
-rm -rf self-hosted
 printf "\x1b[1;33mDone! Have fun with self hosted! \x1b[m\n"
