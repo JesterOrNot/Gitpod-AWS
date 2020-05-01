eeb5f5e4d3cb0bd53c055c9adef6055377f3f87d
Sean Hellum <seanhellum45@gmail.com>
2020-05-01 01:01:41 +0000

    Add more worker nodes because Gitpod needs at least 3

--- src/workers.tf
+++ src/workers.tf
@@ -5,9 +5,9 @@ resource "aws_eks_node_group" "gitpod" {
   subnet_ids      = aws_subnet.gitpod[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
   }
 
   depends_on = [
