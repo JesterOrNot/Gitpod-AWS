3fbda50198f06c7b7007d891429c2163031cb7ba
Sean Hellum <seanhellum45@gmail.com>
2020-05-04 23:01:47 +0000

    Refactor gitpod config

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -x
+set -ex
 if [ -z $(which helm) ]; then
     wget "https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz" \
     && tar xvf helm-v3.0.2-linux-amd64.tar.gz \
-    && sudo mv linux-amd64/helm /usr/local/bin/
+    && sudo install linux-amd64/helm /usr/local/bin/
 fi
 if [ -z $(which kubectl) ]; then
     sudo apt-get install -y apt-transport-https
@@ -11,6 +11,7 @@ if [ -z $(which kubectl) ]; then
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update
     sudo apt-get install -y kubectl
+    printf '\nsource <(kubectl completion bash)\n' >>~/.bashrc
 fi
 if [ -z $(which aws) ]; then
     cd /tmp
@@ -19,9 +20,7 @@ if [ -z $(which aws) ]; then
     sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     sudo unzip awscliv2.zip \
     sudo ./aws/install
-    cd ..
-    sudo rm -rf awscli
-    bash -c "echo \"complete -C '/usr/local/bin/aws_completer' aws\" > ~/.bashrc"
+    echo -e "\ncomplete -C '/usr/local/bin/aws_completer' aws" >>~/.bashrc
 fi
 if [ -z $(which terraform) ]; then
     cd /tmp
@@ -30,8 +29,9 @@ if [ -z $(which terraform) ]; then
     sudo apt-get install unzip
     sudo wget "https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip"
     sudo unzip terraform_0.12.18_linux_amd64.zip
-    cd ..
-    sudo rm -rf terraform
+    chmod +x terraform
+    sudo install terraform /usr/bin
+    printf '\nterraform -install-autocomplete &>/dev/null\n' >>~/.bashrc
 fi
 if [ -z $(which git) ]; then
     sudo apt-get install -y git
