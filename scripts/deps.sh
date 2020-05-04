29f4ea11d4214ce89bcc7fb34d9dd61b0f401b22
Restyled.io <commits@restyled.io>
2020-05-04 23:38:49 +0000

    Restyled by shfmt

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 set -ex
 if [ -z "$(which helm)" ]; then
-    wget "https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz" \
-    && tar xvf helm-v3.0.2-linux-amd64.tar.gz \
-    && sudo install linux-amd64/helm /usr/local/bin/
+  wget "https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz" &&
+    tar xvf helm-v3.0.2-linux-amd64.tar.gz &&
+    sudo install linux-amd64/helm /usr/local/bin/
 fi
 if [ -z "$(which kubectl)" ]; then
-    sudo apt-get install -y apt-transport-https
-    curl -s "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo apt-key add -
-    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-    sudo apt-get update
-    sudo apt-get install -y kubectl
-    printf '\nsource <(kubectl completion bash)\n' >>~/.bashrc
+  sudo apt-get install -y apt-transport-https
+  curl -s "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  sudo apt-get update
+  sudo apt-get install -y kubectl
+  printf '\nsource <(kubectl completion bash)\n' >>~/.bashrc
 fi
 if [ -z "$(which aws)" ]; then
-    brew install awscli
+  brew install awscli
 fi
 if [ -z "$(which terraform)" ]; then
-    cd /tmp
-    sudo mkdir terraform
-    cd terraform
-    sudo apt-get install unzip
-    sudo wget "https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip"
-    sudo unzip terraform_0.12.18_linux_amd64.zip
-    chmod +x terraform
-    sudo install terraform /usr/bin
-    printf '\nterraform -install-autocomplete &>/dev/null\n' >>~/.bashrc
+  cd /tmp
+  sudo mkdir terraform
+  cd terraform
+  sudo apt-get install unzip
+  sudo wget "https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip"
+  sudo unzip terraform_0.12.18_linux_amd64.zip
+  chmod +x terraform
+  sudo install terraform /usr/bin
+  printf '\nterraform -install-autocomplete &>/dev/null\n' >>~/.bashrc
 fi
 if [ -z "$(which git)" ]; then
-    sudo apt-get install -y git
+  sudo apt-get install -y git
 fi
